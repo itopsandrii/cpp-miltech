cc77f59f3a2eabd066c6685b9b9e61fa5ed19f95
Andrii Shavshyn <itopsandrii@gmail.com>
2026-06-01 13:06:01 +0000

    Add homework_06 ballistics: CMake structure, domain logic and CLI

---
+++ homework_06/include/ballistics.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+struct BallisticsInput {
+    double drone_x;
+    double drone_y;
+    double drone_z;
+    double target_x;
+    double target_y;
+    double attack_speed;
+    double acceleration_path;
+    std::string ammo_name;
+};
+
+struct DropSolution {
+    double fire_x;
+    double fire_y;
+};
+
+DropSolution compute_drop_solution(const BallisticsInput& input);
